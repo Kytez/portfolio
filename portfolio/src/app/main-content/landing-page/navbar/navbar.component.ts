a26b57e2d982973f5c44e451920eeb5e0b9dacb8
Kytez <161442764+Kytez@users.noreply.github.com>
2024-07-01 11:49:48 +0200

    added animation burger

--- portfolio/src/app/main-content/landing-page/navbar/navbar.component.ts
+++ portfolio/src/app/main-content/landing-page/navbar/navbar.component.ts
@@ -5,8 +5,39 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  images = [
+    '../../../../assets/img/icons/burger_menu/burger_1.png',
+    '../../../../assets/img/icons/burger_menu/burger_2.png',
+    '../../../../assets/img/icons/burger_menu/burger_3.png',
+    '../../../../assets/img/icons/burger_menu/burger_4.png',
+    '../../../../assets/img/icons/burger_menu/burger_x.png',
+  ];
 
+  currentImage = this.images[0];
+  imageIndex = 0;
+  animating = false;
+
+  onBurgerClick() {
+    if (this.animating) return;
+
+    this.animating = true;
+    this.animateImages(this.imageIndex === 0);
+  }
+
+  animateImages(imgState: boolean) {
+    let step = 0;
+    const interval = setInterval(() => {
+      if (step < this.images.length - 1) {
+        this.imageIndex = imgState ? this.imageIndex + 1 : this.imageIndex - 1;
+        this.currentImage = this.images[this.imageIndex];
+        step++;
+      } else {
+        clearInterval(interval);
+        this.animating = false;
+      }
+    }, 40);
+  }
 }
