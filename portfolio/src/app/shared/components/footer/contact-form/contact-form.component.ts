ddb31146a495dbe558e56ce0da89b4e9ebf67f62
Kytez <161442764+Kytez@users.noreply.github.com>
2024-07-05 15:34:53 +0200

    form finished

--- portfolio/src/app/shared/components/footer/contact-form/contact-form.component.ts
+++ portfolio/src/app/shared/components/footer/contact-form/contact-form.component.ts
@@ -1,18 +1,61 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact-form.component.html',
-  styleUrl: './contact-form.component.scss'
+  styleUrl: './contact-form.component.scss',
 })
 export class ContactFormComponent {
 
-  onSubmit() {
-    console.log('läuft');
-    
+  http = inject(HttpClient);
+
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+    privacyPolicy: false
+  };
+
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://marcel-luwinski.dev/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http
+        .post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      ngForm.resetForm();
+    }
+  }
+
+  toggleCheckbox() {
+    this.contactData.privacyPolicy = !this.contactData.privacyPolicy;
   }
 
 }
+
+
