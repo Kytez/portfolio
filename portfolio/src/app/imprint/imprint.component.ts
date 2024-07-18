d610f89478075f23d6fe9ab7cf4964dafb24bfe2
Kytez <161442764+Kytez@users.noreply.github.com>
2024-07-18 12:42:51 +0200

    updated translation

--- portfolio/src/app/imprint/imprint.component.ts
+++ portfolio/src/app/imprint/imprint.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../main-content/landing-page/navbar/navbar.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, TranslateModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
 export class ImprintComponent {
+
+  constructor(public translateService: TranslateService) {};
 
 }
