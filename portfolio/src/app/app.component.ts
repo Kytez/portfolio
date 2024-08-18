cb8e7eaa7ec226889784e55d36fa06e244037591
Kytez <161442764+Kytez@users.noreply.github.com>
2024-08-18 14:44:16 +0200

    added name animation

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -9,14 +9,16 @@ import { TranslateService } from '@ngx-translate/core';
   standalone: true,
   imports: [CommonModule, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
+
 export class AppComponent {
+  title = 'portfolio';
 
   constructor(private translateService: TranslateService) {
     this.translateService.setDefaultLang('en');
     this.translateService.use(localStorage.getItem('language') || 'en');
   }
 
-  title = 'portfolio';
+
 }
--- portfolio/src/app/shared/components/footer/footer.component.ts
+++ portfolio/src/app/shared/components/footer/footer.component.ts
@@ -2,16 +2,21 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [FormsModule, ContactFormComponent, TranslateModule],
+  imports: [FormsModule, ContactFormComponent, TranslateModule, RouterLink, RouterModule, RouterLinkActive],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
 
   constructor(private translateService: TranslateService) {};
+
+  scrollToTop() {
+    window.scrollTo(0, 0);
+  }
 
 }
